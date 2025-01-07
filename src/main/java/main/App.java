5b8233f6ad35d9ac5243c0e36749c826f0020c76
jpslf <joona.pullinen@student.lut.fi>
2025-01-08 00:35:17 +0200

    Add Animal class and impl main

--- src/main/java/main/App.java
+++ src/main/java/main/App.java
@@ -1,13 +1,21 @@
 package main;
 
 /**
- * Hello world!
- *
- */
-public class App 
+* Hello world!
+*
+*/
+public class App
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+	public static void main( String[] args )
+	{
+		Animal animal = new Animal(
+			"animal",
+			"Animal",
+			1
+		);
+
+		System.out.println(animal.name);
+		System.out.println(animal.species);
+		System.out.println(animal.age);
+	}
 }
